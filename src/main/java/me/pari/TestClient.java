7f8ccec2d6c482cc00454e06d8a72574f192e3f3
Luca <54636488+iimrudy@users.noreply.github.com>
2022-05-17 22:44:49 +0200

    aaa

--- src/main/java/me/pari/TestClient.java
+++ src/main/java/me/pari/TestClient.java
@@ -1,21 +1,29 @@
 package me.pari;
 
+import java.io.PrintWriter;
 import java.net.Socket;
 
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class TestClient {
 
     public static void main(String[] args) {
 
         try (Socket socket = new Socket("127.0.0.1", 7777)) {
-            DataOutputStream output = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-            output.writeBytes("{\"ciao\": \"Ciao\"}");
+            Thread.sleep(1000);
+            DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
+            writer.writeInt(100);
+            writer.flush();
+            writer.writeUTF("diocane");
+            writer.flush();
+            while (true);
 
         }catch (Exception ex) {
             ex.printStackTrace();
         }
+
 
     }
 }
--- src/main/java/me/pari/types/Client.java
+++ src/main/java/me/pari/types/Client.java
@@ -1,10 +1,9 @@
 package me.pari.types;
 
 import com.google.gson.Gson;
+import java.nio.charset.StandardCharsets;
 import me.pari.Server;
-import netscape.javascript.JSObject;
 import org.hydev.logger.HyLogger;
-import org.json.JSONObject;
 
 import java.io.*;
 import java.net.Socket;
@@ -27,12 +26,12 @@ public class Client extends Thread {
 
     // Current client socket info
     private final Socket socket;
-    private final DataOutputStream output;
+    private final BufferedOutputStream output;
     private final DataInputStream input;
 
     public Client(Socket socket) throws IOException {
         this.socket = socket;
-        this.output = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        this.output = new BufferedOutputStream(socket.getOutputStream());
         this.input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
         synchronized (this) {
@@ -44,33 +43,30 @@ public class Client extends Thread {
     @Override
     public void run() {
         Server server = Server.getInstance();
-        while (isConnected.get()) {
+        System.out.println("AAAAA");
+         while (isConnected.get()) {
             try {
                 // Read bytes from the client
-                byte[] bytes = input.readAllBytes();
-                System.out.println(Arrays.toString(bytes));
 
-                // The client disconnected...
-                if (bytes.length == 0) {
-                    LOGGER.log("Client disconnected: " + socket.getRemoteSocketAddress());
-                    close();
-                    return;
-                }
-
-                // Serialize to a readable packet
-                Packet p = new Gson().fromJson(new String(bytes), Packet.class);
+                int x = input.readInt();
+                System.out.println(x);
+                String test = input.readUTF();
+                System.out.println(test);
+                //Packet p = new Gson().fromJson(new String(buffer), Packet.class);
 
                 // Handle the packet by the server
-                server.handle(p);
+                //System.out.println(p);
 
-            } catch (IOException | InterruptedException e) {
+            } catch (IOException e) {
+                close();
                 LOGGER.log("Error reading bytes: " + e.getMessage());
             }
         }
     }
 
     public void sendData(Packet data) throws IOException {
-        output.writeBytes(data.toString());
+        output.write(data.toString().getBytes(StandardCharsets.UTF_8));
+        output.flush();
     }
 
     public synchronized void close() {
